865c1f0c8588ab4d7ce0426acf825221eca75d55
neilmao <neil.xy.mao@gmail.com>
2015-06-23 08:51:54 +1000

    a temp commit

--- sydads-service/src/test/java/com/sydads/service/UserServiceImplTest.java
+++ sydads-service/src/test/java/com/sydads/service/UserServiceImplTest.java
@@ -2,7 +2,10 @@ package com.sydads.service;
 
 import com.sydads.data.User;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
@@ -12,7 +15,8 @@ import static junit.framework.Assert.assertTrue;
  * User: neilmao
  * Date: 22/06/15
  */
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext-mock.xml")
 public class UserServiceImplTest {
 
     private String testingEmail = "1@1.com";
