2c8aa63ea07fa24630216b44af334ac6bb6d20e9
neilmao <neil.xy.mao@gmail.com>
2015-07-07 01:30:50 +1000

    change the sql script

--- sydads-repository/src/main/resources/script.sql
+++ sydads-repository/src/main/resources/script.sql
@@ -1,2 +1,2 @@
-insert into adsuser (id, email, password, status) values ('1','1@1.com', 'test', 1);
+insert into adsuser (id, email, password, status, class) values ('1','1@1.com', 'test', 1, 'Admin');
 insert into user_roles values ('1', 0);
